68bad9cd65adf66f13f3f581ef50f04bfa7698d9
unknown <sabilihaqrafli@gmail.com>
2022-01-06 16:26:20 +0700

    first commit

---
+++ app/src/main/java/lat/pam/cafeauth/SignupActivity.java
@@ -0,0 +1,14 @@
+package lat.pam.cafeauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
